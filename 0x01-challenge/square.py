d0b73fd0110138864e276edb6672df1eea840af6
eneje promise <enejepromise@gmail.com>
2024-07-23 16:50:43 -0700

    fix_the code

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
+"""
+Contains a square class
+"""
+
 
 class square():
-    
-    width = 0
-    height = 0
+    """
+    Square class definition
+    """
 
-    
+    width = 0
+
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
@@ -15,14 +20,15 @@ class square():
         return self.width * self.width
 
     def PermiterOfMySquare(self):
-        return (self.width * 2) + (self.height * 2)
+        return self.width * 4
 
     def __str__(self):
-        return "{}/{}".format(self.width, self.height)
+        return "{}/{}".format(self.width, self.width)
+
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
+    s = square(width=12)
     print(s)
     print(s.area_of_my_square())
     print(s.PermiterOfMySquare())
